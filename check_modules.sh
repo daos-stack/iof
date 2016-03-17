be7b3018767509aceef7ce0fa564657499d3845d
Ashley Pittman <ashley.m.pittman@intel.com>
2016-03-17 17:19:55 +0000

    Ensure that all python files are checked for correctness.
    Add a file that wasn't checked and ensure that new files added to the
    proto directory are checked in the same way as the build picks up
    new files.
    
    Fix errors in the previously unchecked file.
    
    Change-Id: I9d9c8e710ca170ecf8b40c3b6b08f1eec96d0f98
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>
    Reviewed-on: http://review.whamcloud.com/18989
    Tested-by: Jenkins
    Reviewed-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- check_modules.sh
+++ check_modules.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-./scons_local/check_python.sh -s SConstruct \
-                              -s proto/ping/SConscript
+
+set -e
+
+./scons_local/check_python.sh -s SConstruct
+./scons_local/check_python.sh -s src/SConscript
+
+FILE=`ls -1 proto/*/SConscript`
+for FNAME in $FILE
+do
+    ./scons_local/check_python.sh -s $FNAME
+done
