43d16e27cd9c8a756148ee4c29f1974a057657c5
Jeff Olivier <jeffrey.v.olivier@intel.com>
2016-04-06 17:40:47 -0600

    Add hostfile option with enough slots to run tests
    Use mercury-update-scratch and ompi-update-scratch
    
    Change-Id: I0b5a56956c9ac17d7b040863de4c1eebb3077f5d
    Signed-off-by: Jeff Olivier <jeffrey.v.olivier@intel.com>
    Reviewed-on: http://review.whamcloud.com/19364
    Tested-by: Jenkins
    Reviewed-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- build_iof.sh
+++ build_iof.sh
@@ -3,8 +3,11 @@
 set -e
 set -x
 
-prefix=`readlink -f /scratch/coral/artifacts/scons-local-update-scratch/latest`
+#Links are resolved by prereq_tools and target is saved
+MERCURY=${CORAL_ARTIFACTS}/mercury-update-scratch/latest
+OMPI=${CORAL_ARTIFACTS}/ompi-update-scratch/latest
+
 rm -f *.conf
-scons PREBUILT_PREFIX=$prefix -c
+scons PREBUILT_PREFIX=${MERCURY}:${OMPI} -c
 scons
 scons install
--- test_iof.sh
+++ test_iof.sh
@@ -8,23 +8,29 @@ set -x
 echo Trying to run Mercury ping test.
 
 if [ -n "$IOF_USE_VALGRIND" ]; then
-  CMD_PREFIX="valgrind --xml=yes --xml-file=valgrind.%q{PMIX_ID}.xml --leak-check=yes"
+CMD_PREFIX="valgrind --xml=yes --xml-file=valgrind.%q{PMIX_ID}.xml "
+CMD_PREFIX="$CMD_PREFIX --leak-check=yes"
 fi
 
-orterun --tag-output -np 1  $CMD_PREFIX test_rpc_server : \
+echo `hostname` slots=8 > hostfile
+orterun --hostfile hostfile --tag-output -np 1  $CMD_PREFIX test_rpc_server : \
 	-np 1 $CMD_PREFIX test_rpc_client
 
 echo Trying to run process set tests.
 
-orterun --tag-output -np 4 $CMD_PREFIX test_ps --name service_set --is_service 1 : \
-	-np 4 $CMD_PREFIX test_ps --name client_set --is_service 0 --attach_to service_set
+orterun --hostfile hostfile --tag-output -np 4 $CMD_PREFIX test_ps \
+        --name service_set --is_service 1 : \
+	    -np 4 $CMD_PREFIX test_ps --name client_set --is_service 0 \
+        --attach_to service_set
 
-orterun --tag-output -np 1 $CMD_PREFIX test_ps --name a --is_service 1 : \
+orterun --hostfile hostfile --tag-output -np 1 $CMD_PREFIX test_ps \
+    --name a --is_service 1 : \
 	-np 1 $CMD_PREFIX test_ps --name b --attach-to c : \
 	-np 1 $CMD_PREFIX test_ps --name a --is_service 1 : \
 	-np 2 $CMD_PREFIX test_ps --name c --is_service --attach-to a
 
-orterun --tag-output -np 4 $CMD_PREFIX test_ps --name test_srv_set --is_service 1
+orterun --hostfile hostfile --tag-output -np 4 $CMD_PREFIX test_ps \
+    --name test_srv_set --is_service 1
 
 set +e
 
