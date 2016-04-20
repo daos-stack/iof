407cd21895b946f27cc34809d5e477a7e19ae32d
Yulu Jia <yulu.jia@intel.com>
2016-04-20 09:04:17 -0600

    fixes the iof-update-scratch job
    Change-Id: I3d4bdf2ce0ce2123c0b4f6aac0c9c4ad8f686d62
    Signed-off-by: Yulu Jia <yulu.jia@intel.com>
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>
    Reviewed-on: http://review.whamcloud.com/19672
    Tested-by: Jenkins

--- build_iof.sh
+++ build_iof.sh
@@ -3,11 +3,20 @@
 set -e
 set -x
 
+option=
+IOF_INSTALL=
 #Links are resolved by prereq_tools and target is saved
 MERCURY=${CORAL_ARTIFACTS}/mercury-update-scratch/latest
 OMPI=${CORAL_ARTIFACTS}/ompi-update-scratch/latest
 
-rm -f *.conf
-scons PREBUILT_PREFIX=${MERCURY}:${OMPI} -c
-scons
+if [ -n "$WORKSPACE" ]; then
+export IOF_INSTALL="${CORAL_ARTIFACTS}/${JOB_NAME}/${BUILD_NUMBER}"
+PREBUILT_AREA=${MERCURY}:${OMPI}
+option="PREBUILT_PREFIX=${PREBUILT_AREA} PREFIX=${IOF_INSTALL}/iof"
+fi
+scons $option
 scons install
+
+if [ -n "${IOF_INSTALL}" ]; then
+ln -sfn ${IOF_INSTALL} ${CORAL_ARTIFACTS}/${JOB_NAME}/latest
+fi
