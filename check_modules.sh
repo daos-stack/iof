f077bfe6ed2659c3395f402af485604950b8caad
mmennigx <martinx.mennig@intel.com>
2016-07-07 14:38:00 -0600

    CORFSHIP-162 - update iof check module
    remove not need set
    update iof check module to collect all files before calling check_python
    
    Change-Id: I62a948b2cbba78c993e266bbd86f4354fbcd590a
    Signed-off-by: mmennigx <martinx.mennig@intel.com>
    Reviewed-on: http://review.whamcloud.com/21197
    Tested-by: Jenkins
    Reviewed-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- check_modules.sh
+++ check_modules.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-set -e
 
-./scons_local/check_python.sh -s SConstruct
-./scons_local/check_python.sh -s src/SConscript
-
-FILE=`ls -1 test/*.py`
-for FNAME in $FILE
-do
-  ./scons_local/check_python.sh -P3 $FNAME
-done
+FLIST="-s SConstruct -s src/SConscript -s src/utest/SConscript"
 
 FILE=`ls -1 proto/*/SConscript`
 for FNAME in $FILE
 do
-    ./scons_local/check_python.sh -s $FNAME
+  FLIST+=" -s $FNAME"
 done
+
+FILE=`ls -1 test/*.py`
+for FNAME in $FILE
+do
+  FLIST+=" -P3 $FNAME"
+done
+
+./scons_local/check_python.sh $FLIST
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+exit 0
