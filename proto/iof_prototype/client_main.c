00e5131c48058614a666a4960f9d9225a3433806
Ashley Pittman <ashley.m.pittman@intel.com>
2016-08-18 10:28:34 +0100

    Fix a compile issue on OS X.
    Change-Id: I778f7900d60a2d128c214b0a8d902a467442b3ed
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>
    Reviewed-on: http://review.whamcloud.com/21991
    Tested-by: Jenkins
    Reviewed-by: Aditi Gaur <aditi.gaur@intel.com>

--- proto/iof_prototype/client_main.c
+++ proto/iof_prototype/client_main.c
@@ -79,7 +79,7 @@ void get_my_time(struct timespec *ts)
 	clock_get_time(cclock, &mts);
 	mach_port_deallocate(mach_task_self(), cclock);
 	ts->tv_sec = mts.tv_sec;
-	ts_tv_nsec = mts.tv_nsec;
+	ts->tv_nsec = mts.tv_nsec;
 #else
 	clock_gettime(CLOCK_MONOTONIC, ts);
 #endif
