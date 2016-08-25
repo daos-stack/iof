166f1ca5c95052efb40ee61d59f38a7037e17f6c
mmennigx <martinx.mennig@intel.com>
2016-08-25 16:40:41 -0600

    CORFSHIP-218 add test mode configuration
    Change-Id: I731d64f37e35af43d644612d8afe75c75d49020f
    Signed-off-by: mmennigx <martinx.mennig@intel.com>
    Reviewed-on: http://review.whamcloud.com/22144
    Tested-by: Jenkins
    Reviewed-by: Jeff Olivier <jeffrey.v.olivier@intel.com>

--- run_test.sh
+++ run_test.sh
@@ -3,14 +3,18 @@ set -e
 
 # Run the tests from the Jenkins build job so that if there is a test failure
 # the "latest" symlink is not created.
-if [ -n "$IOF_USE_VALGRIND" ]; then
-  scons utest --utest-mode=memcheck
-  python3 test/iof_test_fs.py memcheck
-  mv ${WORKSPACE}/testLogs/test_fs_*/valgrind.*.xml .
-  python3 test/iof_test_ionss.py memcheck
-  mv ${WORKSPACE}/testLogs/nss_*/valgrind.*.xml .
-else
+if [ -z "$IOF_TEST_MODE"  ]; then
+  IOF_TEST_MODE="native"
+fi
+
+if [[ "$IOF_TEST_MODE" =~ (native|all) ]]; then
   scons utest
   python3 test/iof_test_fs.py
   python3 test/iof_test_ionss.py
+fi
+
+if [[ "$IOF_TEST_MODE" =~ (memcheck|all) ]]; then
+  scons utest --utest-mode=memcheck
+  python3 test/iof_test_fs.py memcheck
+  python3 test/iof_test_ionss.py memcheck
 fi
