e8e2d6a8978d7964a2449f00f9f0c77381d9f757
Ashley Pittman <ashley.m.pittman@intel.com>
2017-10-26 11:03:30 +0100

    CART-279 headers: Include ctype.h directly in ionss.
    This header file is required to compile ionss and is being removed
    from cart.
    
    Change-Id: If8819744256cf085a87cc35b25c37d2a477f9503
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>
    Reviewed-on: https://review.whamcloud.com/29792
    Reviewed-by: Jeff Olivier <jeffrey.v.olivier@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>

--- src/ionss/ionss.c
+++ src/ionss/ionss.c
@@ -35,6 +35,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#include <ctype.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
